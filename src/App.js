b12e912704e9b1c42ebaa5eeeecf03c2f5c04d2e
ignacio Quirós <nachoqs@gmail.com>
2020-10-30 08:39:10 -0600

    study github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1>RSVP</h1>
+                    <p>A treehouse App</p>
+                    <form>
+                        <input type="text" value="Safia" placeholder="Invite Someone"/>
+                        <button type="submit" name="submit" value="submit">Submit</button>
+                    </form>
+                </header>
+
+                <div className="main">
+                    <div>
+                        <h2>Invitees</h2>
+                        <label>
+                            <input type="checkbox" /> Hide those who haven't responded
+                        </label>
+                    </div>
+                    <table className="counter">
+                        <tbody>
+                            <tr>
+                                <td>Attending:</td>
+                                <td>2</td>
+                            </tr>
+                            <tr>
+                                <td>Unconfirmed:</td>
+                                <td>1</td>
+                            </tr>
+                            <tr>
+                                <td>Total:</td>
+                                <td>3</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+          );
+    }
 }
 
 export default App;
